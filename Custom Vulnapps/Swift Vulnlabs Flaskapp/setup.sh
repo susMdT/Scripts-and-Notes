819ff80bde3179f06e411d61797b65049ebbf511
susMdT <68256613+susMdT@users.noreply.github.com>
2022-04-02 01:45:43 -0700

    Update setup.sh

--- Custom Vulnapps/Swift Vulnlabs Flaskapp/setup.sh
+++ Custom Vulnapps/Swift Vulnlabs Flaskapp/setup.sh
@@ -3,6 +3,6 @@ apt update ; apt install subversion python3 python-pip -y
 svn export 'https://github.com/susMdT/Scripts-and-Notes/trunk/Custom Vulnapps/Swift Vulnlabs Flaskapp'
 pip install flask
 mv 'Swift Vulnlabs Flaskapp' flaskapp
-cd flaskapp ; rm setup.sh ; cd .. ; rm $(basename "$0")
+cd flaskapp ; rm setup.sh ; mkdir uploads ; cd .. ; rm $(basename "$0")
 cd flaskapp ; env WERKZEUG_DEBUG_PIN=off python3 app.py
 
